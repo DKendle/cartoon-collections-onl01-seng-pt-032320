4a3146ccee03e7b35e30439f7a68e2069f944238
Dina Kendle <dina.kendle@gmail.com>
2020-04-03 14:19:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -39,7 +39,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
  
   if array.any? do |item|
-   ("cheddar","gouda","camembert").include?(item) 
+   ("cheddar""gouda""camembert").include?(item) 
    "#{item}"
     end
   end
