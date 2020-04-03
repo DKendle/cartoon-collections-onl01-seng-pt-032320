5a68f7a41ee7234f30add0ee84a467d1116dde7f
Dina Kendle <dina.kendle@gmail.com>
2020-04-03 14:38:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -40,7 +40,7 @@ def find_the_cheese(array)
   a_3 = []
   
    array.find do |item|
-   a_3 << if array.include?(cheese_types)
+   a_3 << if array.include?(item == cheese_types)
   end
 
   binding.pry
